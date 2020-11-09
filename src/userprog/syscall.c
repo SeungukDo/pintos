572964d4d2a132e4bb5c2d4940f38b4986dd08dd
SeungukDo <hu556727@gmail.com>
2020-11-09 18:29:22 +0000

    modified

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -23,14 +23,21 @@ syscall_handler(struct intr_frame *f UNUSED)
   {
   case SYS_HALT:
     printf("SYS HALT");
+    halt();
     break;
   case SYS_EXIT:
+    check_address(esp + 4);
+    exit(*(uint32_t *)(esp + 4));
     printf("SYS_EXIT");
     break;
   case SYS_EXEC:
+    check_address(esp + 4);
+    exec((const char *)*(uint32_t *)(esp + 4));
     printf("SYS_EXEC");
     break;
   case SYS_WAIT:
+    check_address(esp + 4);
+    wait((pid_t) * (uint32_t *)(esp + 4));
     printf("SYS_WAIT");
     break;
   case SYS_CREATE:
@@ -46,6 +53,10 @@ syscall_handler(struct intr_frame *f UNUSED)
     printf("SYS_FILESIZE");
     break;
   case SYS_READ:
+    check_address(esp + 20);
+    check_address(esp + 24);
+    check_address(esp + 28);
+    read((int)*(uint32_t *)(esp + 20), (void *)*(uint32_t *)(esp + 24), (unsigned)*((uint32_t *)(esp + 28)));
     printf("SYS_READ");
     break;
   case SYS_WRITE:
@@ -66,4 +77,59 @@ syscall_handler(struct intr_frame *f UNUSED)
   }
   printf("system call!\n");
   thread_exit();
+}
+
+void halt(void)
+{
+  shutdown_power_off();
+}
+
+void exit(int status)
+{
+  thread_exit();
+}
+
+pid_t exec(const char *cmd_line)
+{
+  return process_execute(cmd_line);
+}
+
+int wait(pid_t pid)
+{
+  return process_wait(pid);
+}
+
+int read(int fd, void *buffer, unsigned size)
+{
+  int i;
+  if (fd == 0)
+  {
+    for (i = 0; i < size; i++)
+    {
+      if (((char *)buffer)[i] == '\0')
+      {
+        break;
+      }
+    }
+  }
+  return i;
+}
+
+int write(int fd, const void *buffer, unsigned size)
+{
+
+  if (fd == 1)
+  {
+    putbuf(buffer, size);
+    return size;
+  }
+  return -1;
+}
+
+void check_address(void *addr)
+{
+  if (!is_user_vaddr(addr))
+  {
+    exit(-1);
+  }
 }
