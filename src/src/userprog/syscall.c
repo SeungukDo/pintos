bcdc9e83bac5fdca2150f4eb69624d48fc734faf
DESKTOP-MHPGQNN\yujin <jyj7913@postech.ac.kr>
2020-11-12 02:59:25 +0900

    until wait

---
+++ src/src/userprog/syscall.c
@@ -0,0 +1,174 @@
+#include "userprog/syscall.h"
+#include <stdio.h>
+#include <syscall-nr.h>
+#include "threads/interrupt.h"
+#include "threads/thread.h"
+
+static void syscall_handler(struct intr_frame *f UNUSED);
+
+void check_address(void *addr);
+void get_argument(void *esp, int *arg, int count);
+
+void syscall_init(void)
+{
+  intr_register_int(0x30, 3, INTR_ON, syscall_handler, "syscall");
+}
+
+struct lock file_lock;
+
+static void
+syscall_handler(struct intr_frame *f UNUSED)
+{
+  void *esp = f->esp;
+  int number = *(int *)esp;
+  //printf("\nsyscall %d\n", number);
+  //hex_dump(f->esp, f->esp, 100, 1);
+  switch (number)
+  {
+  case SYS_HALT:
+    //printf("\nSYS HALT\n");
+    halt();
+    break;
+  case SYS_EXIT:
+    //printf("\nSYS_EXIT\n");
+
+    check_address(esp + 4);
+    exit(*(uint32_t *)(esp + 4));
+    break;
+  case SYS_EXEC:
+    //printf("\nSYS_EXEC\n");
+
+    check_address(esp + 4);
+    exec((const char *)*(uint32_t *)(esp + 4));
+    break;
+  case SYS_WAIT:
+    //printf("\nSYS_WAIT\n");
+
+    check_address(esp + 4);
+    wait((pid_t) * (uint32_t *)(esp + 4));
+    break;
+  case SYS_CREATE:
+    //printf("\nSYS_CREATE\n");
+    break;
+  case SYS_REMOVE:
+    //printf("\nSYS_REMOVE\n");
+    break;
+  case SYS_OPEN:
+    //printf("\nSYS_OPEN\n");
+    break;
+  case SYS_FILESIZE:
+    //printf("\nSYS_FILESIZE\n");
+    break;
+  case SYS_READ:
+    //printf("\nSYS_READ\n");
+
+    check_address(esp + 4);
+    check_address(esp + 8);
+    check_address(esp + 12);
+    read((int)*(uint32_t *)(esp + 4),
+    (void *)*(uint32_t *)(esp + 8),
+    (unsigned)*((uint32_t *)(esp + 12)));
+    break;
+  case SYS_WRITE:
+    //hex_dump(f->esp, f->esp, 100, 1);
+
+    check_address(esp + 4);
+    check_address(esp + 8);
+    check_address(esp + 12);
+    write((int)*(uint32_t *)(f->esp + 4), 
+    (void *)*(uint32_t *)(f->esp + 8), 
+    (unsigned)*((uint32_t *)(f->esp + 12)));
+    break;
+  case SYS_SEEK:
+    //printf("SYS_SEEK");
+    break;
+  case SYS_TELL:
+    //printf("SYS_TELL");
+    break;
+  case SYS_CLOSE:
+    //printf("SYS_CLOSE");
+    break;
+
+  default:
+    break;
+  }
+}
+
+void halt(void)
+{
+  shutdown_power_off();
+}
+
+void exit(int status)
+{
+	printf("%s: exit(%d)\n", thread_current()->name, status);
+  thread_current()->exit_status = status;
+  thread_exit();
+}
+
+pid_t exec(const char *cmd_line)
+{
+  pid_t pid = process_execute(cmd_line);
+  struct thread* child;
+
+  struct list_elem* elem;
+  for(elem = list_begin(&thread_current()->child_list);
+      elem != list_end(&thread_current()->child_list);
+      elem = list_next(elem)){
+
+    child = list_entry(elem, struct thread, child_elem);
+
+    if(child->tid == pid) {
+      sema_down(&child->load_sema);
+
+      if(child->loaded)
+        return pid;
+      else
+        return -1;
+    }
+  }
+  return -1;
+}
+
+int wait(pid_t pid)
+{
+  return process_wait(pid);
+}
+
+int read(int fd, void *buffer, unsigned size)
+{
+  int i;
+  if (fd == 0)
+  {
+    for (i = 0; i < size; i++)
+    {
+      if (((char *)buffer)[i] == '\0')
+      {
+        break;
+      }
+    }
+  }
+  return i;
+}
+
+int write(int fd, const void *buffer, unsigned size)
+{
+  if (fd == 1)
+  {
+    putbuf((const char*)buffer, size);
+    return size;
+  }
+  return -1;
+}
+
+void check_address(void *addr)
+{
+  if((unsigned int)addr >= 0x8048000){
+    if((unsigned int)addr < 0xc0000000){
+      return;
+    }
+  }
+	else{
+		exit(-1);
+	}
+}
